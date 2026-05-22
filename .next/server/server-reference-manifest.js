12b46ce54e4ec2c25943452bec6664308eb31eee
Jubayer Nafim <jubayer@example.com>
2026-05-23 00:04:06 +0600

    Deployment with api intigration

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"IGDvfi2frFvZW9TfJskpEvO1onxG8V8RnCq+NkBhJEo=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"kBnPc+oGRG7mKESRLLZZpvDqek42yu5QWVYGfufqYD4=\"}"
